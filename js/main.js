62413790e50d0db52dd2d52fd9a35b7474810a97
Enrique Dalfo <dalfoenrique@gmail.com>
2022-04-30 11:28:30 -0300

    con js

---
+++ js/main.js
@@ -0,0 +1,82 @@
+/*ndlnsjdnd*/
+//jdosvnofndkjas
+
+/*let nombreUsuario = prompt ("Hola, Bienvenido! Ingresa tu Usuario");
+
+alert("Hola " + nombreUsuario);
+
+console.log("Hola " + nombreUsuario);
+*/
+
+// let nota = prompt("ingresa tu nota");
+
+// if (nota >= 0 && nota < 6) {
+//   console.log("No Aprobado, nos vamos a seguir viendo");} 
+//   else if (nota >= 6 && nota < 10) {
+//   console.log("Bien ahi, aprobaste...");} 
+//   else if (nota == 10) {
+//   console.log("Felicidades nota maxima, sos un crack");} 
+//   else if (nota < 0 || nota > 10) {console.log("ERROR");}
+
+/*
+let sabor = prompt("Elegi tu sabor / Para finalizar escribir - total -");
+
+while(sabor != "total") {
+  switch(sabor) {
+    case "dulce de leche":
+      alert ("Agregaste dulce de leche a tu carrito");
+      console.log("Agregaste dulce de leche a tu carrito");
+      console.count("dulce de leche");
+      break;
+    case "nutella":
+      alert ("Agregaste nutella a tu carrito");
+      console.log("Agregaste nutella a tu carrito");
+      console.count("nutella");
+      break;
+    case "limon":
+      alert ("Agregaste limon a tu carrito");
+      console.log("Agregaste limon a tu carrito");
+      console.count("limon");
+      break;
+    case "frutos rojos":
+      alert ("Agregaste frutos rojos a tu carrito");
+      console.log("Agregaste frutos rojos a tu carrito");
+      console.count("frutos rojos");
+    default: 
+      alert("Eso no es un sabor, elegi tu sabor / Para finalizar escribir - total -");
+      break;
+  }
+  sabor = prompt("Elegi tu sabor / Para finalizar escribir - total -");
+}
+*/
+let sabor = prompt("Elegi tu sabor / Para finalizar escribir - total -");
+let total = 0;
+while(sabor != "total") {
+  switch(sabor) {
+    case "dulce de leche":
+      alert ("Agregaste dulce de leche a tu carrito");
+      console.log("Agregaste dulce de leche a tu carrito");
+      total+=1;
+      break;
+    case "nutella":
+      alert ("Agregaste nutella a tu carrito");
+      console.log("Agregaste nutella a tu carrito");
+      total+=1;
+      break;
+    case "limon":
+      alert ("Agregaste limon a tu carrito");
+      console.log("Agregaste limon a tu carrito");
+      total+=1;
+      break;
+    case "frutos rojos":
+      alert ("Agregaste frutos rojos a tu carrito");
+      console.log("Agregaste frutos rojos a tu carrito");
+      total+=1;
+      break;
+    default: 
+      alert("Eso no es un sabor, elegi tu sabor / Para finalizar escribir - total -");
+      break;
+  }
+  sabor = prompt("Elegi tu sabor / Para finalizar escribir - total -");
+}
+console.log("Cantidad total: " + total);

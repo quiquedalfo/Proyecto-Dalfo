14c312c5446a8812cf7d705ca0ea4a1172b33a64
Enrique Dalfo <dalfoenrique@gmail.com>
2022-05-14 23:30:58 -0300

    trabajando en el js

--- js/main.js
+++ js/main.js
@@ -1,67 +1,128 @@
-const Limon = {
-  sabor: "limon",
-  chocolate: "blanco",
-  color: "amarillo",
-  detalles: "blanco"};
-const Manzana = {
-  sabor: "manzana",
-  chocolate: "blanco",
-  color: "verde",
-  detalles: "rojo"};
-const Nuez = {
-  sabor: "nuez",
-  chocolate: "blanco",
-  color: "blanco",
-  detalles: "naranja"};
-const Caju = {
-  sabor: "caju",
-  chocolate: "blanco",
-  color: "celeste",
-  detalles: "naranja"};
-const DulceDeLeche = {
-  sabor: "dulce de leche",
-  chocolate: "con leche",
-  color: "celeste",
-  detalles: "blanco y amarillo"};
-const Nutella = {
-  sabor: "nutella",
-  chocolate: "con leche",
-  color: "blanco",
-  detalles: "rojo y negro"};
-const FrutosRojos = {
-  sabor: "frutos rojos",
-  chocolate: "con leche",
-  color: "rojo",
-  detalles: "rosa"};
-const Toffee = {
-  sabor: "toffee",
-  chocolate: "con leche",
-  color: "naranja",
-  detalles: "rojo, verde y amarillo"};
-const Cafe = {
-  sabor: "cafe",
-  chocolate: "amargo",
-  color: "blanco",
-  detalles: "marron"};
-const Almendras = {
-  sabor: "almendras",
-  chocolate: "amargo",
-  color: "rojo",
-  detalles: "amrillo"};
-const Naranja = {
-  sabor: "naranja",
-  chocolate: "amargo",
-  color: "naranja",
-  detalles: "verde"};
-const Banana = {
-  sabor: "banana",
-  chocolate: "amargo",
-  color: "amarillo",
-  detalles: "negro"};
-const sabores = [Limon, Manzana, Nuez, Caju, Toffee, Nutella, DulceDeLeche, FrutosRojos, Cafe, Banana, Naranja, Almendras];
+class Bombon {
+  constructor(id, sabor, chocolate, color, color2, codigo,precio) {
+    this.id = id;
+    this.sabor = sabor;
+    this.chocolate = chocolate;
+    this.color = color;
+    this.color2 = color2;
+    this.codigo = codigo;
+    this.precio = precio;
+  }
+}
+;
+//Lista de bombones con Detalles
+const ListaDeBombones = [
+  new Bombon (1,"Limon", "Chocolate Blanco", "Amarillo", "Blanco", "LIM", 120),
+  new Bombon(2,"Manzana", "Chocolate Blanco", "Verde", "Rojo", "MAN", 120),
+  new Bombon(3,"Nuez", "Chocolate Blanco", "Blanco", "Naranja", "NUE", 120),
+  new Bombon(4,"Caju", "Chocolate Blanco", "Celeste", "Naranja", "CAJ", 120),
+  new Bombon(5,"Nutella", "Chocolate con Leche", "Blanco" , "Rojo" , "NUT", 120),
+  new Bombon(6,"Dulce de Leche", "Chocolate con Leche", "Celeste" , "Blanco" , "DDL", 120),
+  new Bombon(7,"Frutos Rojos", "Chocolate con Leche", "Rojo" , "Rosa" , "F.R", 120),
+  new Bombon(8,"Toffee", "Chocolate con Leche", "Naranja" , "Rojo" , "TOF", 120),
+  new Bombon(9,"Naranja", "Chocolate Amargo", "Naranja" , "Verde" , "NAR", 120),
+  new Bombon(10,"Banana", "Chocolate Amargo", "Amarillo" , "Negro" , "BNN", 120),
+  new Bombon(11,"Almendras", "Chocolate Amargo", "Rojo" , "Amarillo" , "ALM", 120),
+  new Bombon(12,"Cafe", "Chocolate Amargo", "Blanco" , "Marron" , "CAF", 120),
+];
 
-const MiCaja = [];
-let sabor = prompt("elegi tu sabor");
-MiCaja.push(sabor);
+const PrecioCajaX6 = 2000;
+const PrecioCajaX12 = 3000;
+const CantidadCajax12 = 12;
+const CantidadCajax6 = 6;
+let cantidad = 0;
+let carrito = [];
 
-console.log(MiCaja);
+
+
+//Indentificacion de usuario
+const Login = () => {
+  let nombre = prompt("Hola, ingresa tu nombre");
+  while (nombre =="" || nombre == null) {
+    nombre = prompt("Ingrese tu nombre correctamente");
+  }
+  return nombre;
+};
+
+//Indentificacion de sabor
+const ElegirSabor = () => {
+  let sabor = prompt("Que sabor queres?");
+  while (sabor == "" && sabor != ListaDeBombones) {
+    sabor = prompt("Que sabor queres?");
+  }
+  return sabor;
+};
+
+//Indentificacion de cantidad
+const ElegirCantidad = () => {
+  let cantidad = parseInt(prompt("Cuantos queres?"));
+  while (cantidad < 1 || cantidad >=20 ) {
+    alert(`Lo lamento pero en numero no es adecuado.\n Por favor ingresa entre 1 el 20`);
+    cantidad = parseInt(prompt("Cuantos queres?"));
+  }
+  return cantidad;
+};
+
+//Ver lista de bombones
+const VerLista = () => {
+  let texto = "";
+  ListaDeBombones.forEach ((element) =>{
+      texto += `${element.id}) ${element.sabor}\n`;
+  })
+  alert(texto);
+};
+
+
+//Buscar si matchea con la lista de bombones 
+
+const BombonesSeleccionados = (sabor) => {
+  console.log(sabor);
+  let BombonFind = ListaDeBombones.find((element) => element.sabor === sabor);
+  carrito.push(BombonFind);
+
+  let seguir = confirm("Elija otro sabor" + VerLista);
+
+  if (seguir === true) {
+    init();
+  }
+};
+
+
+
+
+
+let Cliente = Login();
+let Lista = VerLista();
+let Producto = ElegirSabor();
+let CantidadBruta = ElegirCantidad();
+carrito.push(cantidad);
+
+console.log(Cliente);
+console.log(Producto);
+console.log(CantidadBruta);
+console.log(carrito);
+
+/*let Carrito = CantidadBruta;
+if (Carrito.CantidadBruta != 6) {
+  Producto = ElegirSabor();
+  CantidadBruta = ElegirCantidad();
+} else {
+  console.log(Carrito)
+}*/
+
+/*let Carrito = CantidadBruta;
+if (Carrito.CantidadBruta != 12) {
+  Producto = ElegirSabor();
+  CantidadBruta = ElegirCantidad();
+} else {
+  console.log(Carrito)
+}
+
+
+
+/*queres cajas de 12 o de 25?
+12 o 25
+si es caja de 12 entonces lista y promt de sabor y cantidad
+si es caja de 25 entonces lista y prompt de sabor y cantidad
+Ambos seran enviados a un array para ser mostrado al final de el 
+*/
